6f87d573b0bca5865373e22bd3e59ab3f1abc48b
Margeaux Spring <contact@cheapwebmonkey.com>
2019-06-29 20:57:33 -0400

    inital commit

---
+++ assets/javascript/main.js
@@ -0,0 +1,11 @@
+//carousel
+// $( document ).ready(function() {
+//     $('.main-carousel').flickity({
+//       // options
+//       imagesLoaded: true,
+//       freeScroll: true,
+//       wrapAround: true,
+//       contain: true
+//     });
+//
+// });
